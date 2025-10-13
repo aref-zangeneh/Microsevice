28fd653a4d2c77dc831488750a6b013db478b577
Aref Zangeneh <aref.za.94@gmail.com>
2025-10-14 02:08:54 +0330

    DiscountAPI project added

---
+++ aspnet-microservices/Services/Discount/Discount.Api/Program.cs
@@ -0,0 +1,25 @@
+using Discount.Api.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Discount.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            // migration
+            host.MigrateDatabase<Program>();
+            host.Run();
+            //CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
